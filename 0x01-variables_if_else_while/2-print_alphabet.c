00c773221fc4f023bc44a339d1cadb269a9b935d
Alexkimath <alexkimathi540@gmail.com>
2022-09-10 09:08:50 -0700

    ouputs aphabets in lower case

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+/*outputs alphabets in lowercase*/
+#include <stdio.h>
+/**
+ * main- Entry point
+ *
+ * Description: prints alphabets
+ * printing in lowercase
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	/* start with a */
+	int letter;
+
+	letter = 'a';
+	while letter <= 'z';
+	{ putchar(letter);
+
+	letter++;
+	}
+	putchar('\n');
+		return (0);
