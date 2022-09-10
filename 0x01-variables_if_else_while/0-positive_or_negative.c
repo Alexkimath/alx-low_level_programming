6f18f90ba0ec06f1c2055065a6e9c72aa8907522
Alexkimath <alexkimathi540@gmail.com>
2022-09-10 07:49:24 -0700

    Determines whether positive or negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,12 +3,13 @@
 #include <time.h>
 #include <stdio.h>
 /**
-  * main -Entry point
-  *
-  * Description: Gets a random number and prints
-  * if a number is positive negative or zero
-  * return: Always (0) sucess
-  */
+ * main- determine whether the number is positive or negative
+ *
+ * Description: Gets a random number and prints
+ * if a number is positive negative or zero
+ *
+ * Return: Always (0) sucess
+ */
 
 int main(void)
 {
