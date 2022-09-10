97984e2fa28ec16552c12eb2d99b4af9d98aafee
Alexkimath <alexkimathi540@gmail.com>
2022-09-10 08:03:57 -0700

    using put statement

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
 /*output a string*/
-
 #include<stdio.h>
 /**
-  * main: entry
-  * Description:prints, Programming is like building a multilingual puzzle
-
-  * Return: always 0
-  */
+ * main- where the program starts
+ * Description:prints, Programming is like building a multilingual puzzle
+ *
+ * Return: always 0
+ */
 int main(void)
 
 {	puts("\"Programming is like building a multilingual puzzle");
