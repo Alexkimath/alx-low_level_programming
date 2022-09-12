ef28cfe4d07473ef77625cc46e0eaafc7db9097e
Alexkimath <alexkimathi540@gmail.com>
2022-09-12 02:13:52 -0700

    prints single digt to base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main- entry point
+ *
+ * Return: ALways 0 success
+ */
+int main(void)
+{
+	int ch;
+
+	for (ch > 0 && ch < 10)
+	{
+		putchar(ch);
+	}
+
+	else
+	{
+	}
+	putchar('\n);
+	return (0);
+}
