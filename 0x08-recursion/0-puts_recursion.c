7ba2e07d78f9f0f046e816bf0270fa9924ad30bf
Alexkimath <alexkimathi540@gmail.com>
2022-09-28 11:34:21 -0700

    recusion taks

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _puts_recursion - prints a string
+ *
+ * Returns: Void
+ */
+void _puts_recursion(char *s)
+{
+	int 1 = 0;
+	while (s[i] != '\n' && s[i] != '\0')
+	{
+		putchar(s[i]);
+		i++
+	}
+
+}
---
+++ 0x08-recursion/README.md
@@ -0,0 +1,1 @@
+My recursin readme file
